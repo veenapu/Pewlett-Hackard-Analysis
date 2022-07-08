57a8428e2bc425d10ad5096107d5622cfe75aa89
Veena Puvvada <veenapuvvada@yahoo.com>
2022-07-07 21:25:02 -0400

    adding SQL queries

---
+++ Mod 7 homework.sql
@@ -0,0 +1,19 @@
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 -- Creating tables for PH-EmployeeDB
-CREATE TABLE departments(
+CREATE TABLE departments (
 	dept_no VARCHAR(4) NOT NULL,
 	dept_name VARCHAR(40) NOT NULL,
 	PRIMARY KEY (dept_no),
 	UNIQUE (dept_name)
 );
 
-CREATE TABLE employees(
+CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
     first_name VARCHAR NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE employees(
     PRIMARY KEY (emp_no)
 );	
 
-CREATE TABLE dept_manager(
+CREATE TABLE managers (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
@@ -41,7 +41,8 @@ CREATE TABLE dept_emp (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -50,7 +51,7 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
